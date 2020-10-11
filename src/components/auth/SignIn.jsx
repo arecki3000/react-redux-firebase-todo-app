34e951c329de3bf7515aae479fffac4f88f6cef1
Arkadiusz Kwiatkowski <58771611+arecki3000@users.noreply.github.com>
2020-10-11 17:26:17 +0200

    add bootstrap and react-router-dom

---
+++ src/components/auth/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SignIn = () => {
+  return (
+    <>
+      <h3>Sign In</h3>
+    </>
+  );
+};
---
+++ src/components/auth/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SignUp = () => {
+  return (
+    <>
+      <h3>Sign Up</h3>
+    </>
+  );
+};
