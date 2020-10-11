6b374cfc64419aa7ad47dc47f6010258ab0162d2
Arkadiusz Kwiatkowski <58771611+arecki3000@users.noreply.github.com>
2020-10-11 23:06:14 +0200

    add store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import App from "./App";
+import rootReducer from "./reducers/rootReducer";
+
+const store = createStore(rootReducer);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   rootElement
 );
